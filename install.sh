dad202a04a7f13a3e31a8f62175bf56342e3b7ee
DehengXu <dehengxu@outlook.com>
2015-09-13 17:59:37 +0800

    add install.sh script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+filename=~/.bash_profile
+path="~/.gitbash"
+if [ -e ${path} ]; then
+    # git clone https://github.com/xudeheng/gitbash.git  ${path}
+    echo "git clone https://github.com/xudeheng/gitbash.git  ${path}"
+fi
+
+r=$(find ${filename} | xargs grep "source ~/.gitbash/mygit.rc")
+echo ${r}
+
+if [ -z "$r" ];then
+    echo "Found!";
+    echo "source ~/.gitbash/mygit.rc" >> "${filename}"
+else
+    echo "Has already install!"
+fi

625963e981171b5a6bc1c5a10c864b7ff098f185
CyberLion <dehengxu@outlook.com>
2015-09-14 12:14:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-filename=~/.bash_profile
-path="~/.gitbash"
+filename="${HOME}/.bash_profile"
+path="${HOME}/.gitbash"
 if [ ! -d ${path} ]; then
     git clone https://github.com/xudeheng/gitbash.git  ${path}
     echo "git clone https://github.com/xudeheng/gitbash.git  ${path}"
 fi
 
-r=$(find ${filename} | xargs grep "source ~/.gitbash/mygit.rc")
+r=$(find ${filename} | xargs grep "source ${HOME}/.gitbash/mygit.rc")
 echo ${r}
 
 if [ -z "$r" ];then
     echo "Found!";
-    echo "source ~/.gitbash/mygit.rc" >> "${filename}"
+    echo "source ${HOME}/.gitbash/mygit.rc" >> "${filename}"
 else
     echo "Has already install!"
 fi

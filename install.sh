002e420cd643043936e730c37937811eb8aaa7ae
CyberLion <dehengxu@outlook.com>
2015-10-23 11:48:01 +0800

    Update install.sh
    add Zsh support

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-filename="${HOME}/.bash_profile"
 path="${HOME}/.gitbash"
 if [ ! -d ${path} ]; then
     git clone https://github.com/xudeheng/gitbash.git  ${path}
     echo "git clone https://github.com/xudeheng/gitbash.git  ${path}"
 fi
 
+filename="${HOME}/.bash_profile"
 r=$(find ${filename} | xargs grep "source ${HOME}/.gitbash/mygit.rc")
 echo ${r}
 
@@ -16,6 +16,29 @@ if [ -z "$r" ];then
 else
     echo "Has already install!"
 fi
+
+filename="${HOME}/.bashrc"
+r=$(find ${filename} | xargs grep "source ${HOME}/.gitbash/mygit.rc")
+echo ${r}
+
+if [ -z "$r" ];then
+    echo "Found!";
+    echo 'source ${HOME}/.gitbash/mygit.rc' >> "${filename}"
+else
+    echo "Has already install!"
+fi
+
+filename="${HOME}/.zshrc"
+r=$(find ${filename} | xargs grep "source ${HOME}/.gitbash/mygit.rc")
+echo ${r}
+
+if [ -z "$r" ];then
+    echo "Found!";
+    echo 'source ${HOME}/.gitbash/mygit.rc' >> "${filename}"
+else
+    echo "Has already install!"
+fi
+
 
 source "${HOME}/.gitbash/mygit.rc"
 echo "source $HOME/.gitbash/mygit.rc"
